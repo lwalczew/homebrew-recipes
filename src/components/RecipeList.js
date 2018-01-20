b164901228abc28a9f417a72fa64658a965439bc
Luke Walczewski <luke@schedulingad.com>
2018-01-19 19:31:01 -0500

    no recipes match filters notification

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Accordion, Button, Table} from 'semantic-ui-react';
+import {Accordion, Button, Icon, Table} from 'semantic-ui-react';
 import ShowRecipeModal from './ShowRecipeModal';
 
 const RecipeList = (props) => {
@@ -30,10 +30,10 @@ const RecipeList = (props) => {
                 <Table.Cell style={{verticalAlign: 'top'}}>Yeast:</Table.Cell>
                 <Table.Cell>{recipe.yeast}</Table.Cell>
               </Table.Row>
-              {recipe.otherIngredients[0].name.length ? <Table.Row>
+              {recipe.otherIngredients[0].name.length > 0 && <Table.Row>
                 <Table.Cell style={{verticalAlign: 'top'}}>Other:</Table.Cell>
                 <Table.Cell>{recipe.otherIngredients.map((otherIngredient, i) => <div key={i}>{otherIngredient.name}, {otherIngredient.amount}, {otherIngredient.time}<br/></div>)}</Table.Cell>
-              </Table.Row> : null}
+              </Table.Row>}
               <Table.Row>
                 <Table.Cell style={{verticalAlign: 'top'}}>Directions:</Table.Cell>
                 <Table.Cell>{recipe.directions}</Table.Cell>
@@ -51,7 +51,14 @@ const RecipeList = (props) => {
 
 
   return (
-    <Accordion panels={accordionPanels} exclusive={false} styled/>
+    props.visibleRecipes.length > 0
+      ? <Accordion panels={accordionPanels} exclusive={false} styled/>
+      :
+        <div style={{marginTop: '2em', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+          <Icon name='help circle outline' size='huge' color='orange'/>
+          <h1>No recipes match your selected filters.</h1>
+          <h1>Please clear/change your filters to view recipes.</h1>
+        </div>
   )
 
 }
